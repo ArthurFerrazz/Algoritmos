504aa8964fe03963059bec6bed06e82e53ebdfcc
ArthurFerrazz <111258580+ArthurFerrazz@users.noreply.github.com>
2022-11-26 22:34:53 -0300

    Delete Sequencial directory

--- Sequencial/Q1.cpp
+++
@@ -1,9 +0,0 @@
-/*O seu primeiro programa em qualquer linguagem de programação normalmente é
-o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir
-esta mensagem na tela.*/
-
-#include <stdio.h>
-
-int main(){
-	printf("Hello World!");
-}
